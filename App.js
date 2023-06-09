25953abf68bbed6e04b02b397515a60a206c0174
victormnoel <victor-siqueira01@hotmail.com>
2023-06-09 03:18:11 -0300

    UploadArquivos

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+//import arq
+import api from './src/services/api';
+import Filmes from './src/filmes';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      filmes: []
+    }
+  }
+
+  async componentDidMount() {
+
+    const response = await api.get('r-api/?api=filmes');
+    this.setState({
+      filmes: response.data
+    });
+  }
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.filmes}
+          keyExtractor={item => item.id.toString()}
+          renderItem={({item}) => <Filmes data={item}/>}
+        />
+      </View>
+    )
+  }
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 50
   },
 });
